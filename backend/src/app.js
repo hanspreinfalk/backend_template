d8243296f78c5d04d4bc078eb0cab9e0c5dadb14
hanspreinfalk <hans.preinfalk.davila@gmail.com>
2021-07-26 10:28:07 -0600

    Second try

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,19 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+var corsOptions = {
+    origin: '*',
+  }
+
 app.use(require('./routes/users'));
 
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 module.exports = app;
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -8,12 +8,6 @@ const faker = require('faker');
 router.get('/api/users', async (req, res) => {
     const users = await User.find();
     res.json({users});
-    
-    req.append('Access-Control-Allow-Origin', '*');
-    // res.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    // res.append('Access-Control-Allow-Headers', 'Content-Type');
-    // res.append('Content-Type: application/json');	// CORS
-    req.append("Access-Control-Allow-Headers: Access-Control-Allow-Origin, Accept");
 });
 
 router.get('/api/users/create', async (req, res) => {
@@ -24,7 +18,7 @@ router.get('/api/users/create', async (req, res) => {
             avatar: faker.image.avatar()
         });
     }
-    res.json({message: '5 Users created'});
+    res.json({ message: '5 Users created' });
 });
 
 module.exports = router;
